f344d7ec4c2266521ae4bb202ce16ef1ac456aba
rcaziraghi <rcaziraghi@gmail.com>
2022-10-31 00:03:41 +0000

    Subscription callbacks implementadas.

--- kyma-multitenant-node/routes/index.js
+++ kyma-multitenant-node/routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+/* Variables and constants to help define logic for APIRule creation */
+const EF_SERVICE_NAME = 'kyma-multitenant-approuter-multitenancy';
+const EF_SERVICE_PORT = 8080;
+const EF_APIRULE_DEFAULT_NAMESPACE = 'multitenancy-ns';
+const KYMA_APIRULE_GROUP = 'gateway.kyma-project.io';
+const KYMA_APIRULE_VERSION = 'v1alpha1';
+const KYMA_APIRULE_PLURAL = 'apirules';
+
+const k8s = require('@kubernetes/client-node');
+const createApiRule = require('./createApiRule');
+var kyma_cluster = process.env.CLUSTER_DOMAIN || "UNKNOWN";
+
 /* GET home page. */
 /**
  * This is the default end-point when someone attempts to access the SaaS application.
@@ -23,5 +35,104 @@ var router = express.Router();
       res.send(responseMsg);
   }
 });
+
+//******************************** API Callbacks for multitenancy ********************************
+
+/**
+ * Request Method Type - PUT
+ * When a consumer subscribes to this application, SaaS Provisioning invokes this API.
+ * We return the SaaS application url for the subscribing tenant.
+ * This URL is unique per tenant and each tenant can access the application only through it's URL.
+ */
+ router.put('/callback/v1.0/tenants/*', async function(req, res) {
+    //1. create tenant unique URL
+    var consumerSubdomain = req.body.subscribedSubdomain;
+    var tenantAppURL = "https:\/\/" + consumerSubdomain + "-approuter." + "eba936b";
+
+    //2. create apirules with subdomain,
+    const kc = new k8s.KubeConfig();
+    kc.loadFromCluster();
+    const k8sApi = kc.makeApiClient(k8s.CustomObjectsApi);
+    const apiRuleTempl = createApiRule.createApiRule(
+        EF_SERVICE_NAME,
+        EF_SERVICE_PORT,
+        consumerSubdomain + "-approuter",
+        kyma_cluster);
+
+    try {
+        const result = await k8sApi.getNamespacedCustomObject(KYMA_APIRULE_GROUP,
+            KYMA_APIRULE_VERSION,
+            EF_APIRULE_DEFAULT_NAMESPACE,
+            KYMA_APIRULE_PLURAL,
+            apiRuleTempl.metadata.name);
+        //console.log(result.response);
+        if (result.response.statusCode == 200) {
+            console.log(apiRuleTempl.metadata.name + ' já existe.');
+            res.status(200).send(tenantAppURL);
+        }
+    } catch (err) {
+        //create apirule if non-exist
+        console.warn(apiRuleTempl.metadata.name + ' não existe, criando...');
+        try {
+            const createResult = await k8sApi.createNamespacedCustomObject(KYMA_APIRULE_GROUP,
+                KYMA_APIRULE_VERSION,
+                EF_APIRULE_DEFAULT_NAMESPACE,
+                KYMA_APIRULE_PLURAL,
+                apiRuleTempl);
+            console.log(createResult.response);
+
+            if (createResult.response.statusCode == 201) {
+                console.log("Regra API criada!");
+                res.status(200).send(tenantAppURL);
+            }
+        } catch (err) {
+            console.log(err);
+            console.error("Falha ao criar APIRule");
+            res.status(500).send("Erro ao criar APIRule");
+        }
+    }
+    console.log("saindo do onboarding...");
+    res.status(200).send(tenantAppURL)
+});
+
+/**
+ * Request Method Type - DELETE
+ * When a consumer unsubscribes this application, SaaS Provisioning invokes this API.
+ * We delete the consumer entry in the SaaS Provisioning service.
+ */
+router.delete('/callback/v1.0/tenants/*', async function(req, res) {
+    console.log(req.body);
+    var consumerSubdomain = req.body.subscribedSubdomain;
+
+    //delete apirule with subdomain
+    const kc = new k8s.KubeConfig();
+    kc.loadFromCluster();
+
+    const k8sApi = kc.makeApiClient(k8s.CustomObjectsApi);
+
+    const apiRuleTempl = createApiRule.createApiRule(
+        EF_SERVICE_NAME,
+        EF_SERVICE_PORT,
+        consumerSubdomain + "-approuter",
+        kyma_cluster);
+
+    try {
+        const result = await k8sApi.deleteNamespacedCustomObject(
+            KYMA_APIRULE_GROUP,
+            KYMA_APIRULE_VERSION,
+            EF_APIRULE_DEFAULT_NAMESPACE,
+            KYMA_APIRULE_PLURAL,
+            apiRuleTempl.metadata.name);
+        if (result.response.statusCode == 200) {
+            console.log("Regra API deletada!");
+        }
+    } catch (err) {
+        console.error(err);
+        console.error("Erro ao deletar API Rule");
+    }
+
+    res.status(200).send("deletada");
+});
+//************************************************************************************************
 
 module.exports = router;
